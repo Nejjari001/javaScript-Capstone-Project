f6a593db2b37c3129cead113a11e523d6c71e276
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-11-02 09:33:30 +0100

    Add items to Home page with basic data title and image

---
+++ modules/likes.js
@@ -0,0 +1,22 @@
+const apiUrl =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
+const apiKey = 'eJ0dYt3woHnL6Sz1FyVG';
+const getLikes = async () => {
+  const res = await fetch(`${apiUrl}/${apiKey}/likes`);
+  const data = await res.json();
+  return data;
+};
+export default getLikes;
+export const addLikes = async (id) => {
+  await fetch(
+    ' https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eJ0dYt3woHnL6Sz1FyVG/likes',
+    {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        item_id: id,
+      }),
+    }
+  );
+};
+// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eJ0dYt3woHnL6Sz1FyVG/likes
--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-import './styles.css';
+import getLikes, { addLikes } from '../modules/likes.js';
+import render from '../modules/render.js';
+import '../src/styles.css';
+
+const element = document.querySelector('#container');
+const mealUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+const fetchData = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  const likes = await getLikes();
+  const storage = [];
+  data.categories.forEach((item) => {
+    storage.push({
+      ...(likes.find((innerItem) => innerItem.item_id.toString() === item.idCategory.toString())),
+      ...item,
+    });
+  });
+  return storage;
+};
+
+window.onload = async () => {
+  const storage = await fetchData(mealUrl);
+  render(storage, element);
+  const likeButtons = document.querySelectorAll('.fa-heart');
+  const likeBtns = Array.from(likeButtons);
+  likeBtns.forEach((btn) => {
+    btn.addEventListener('click', async (e) => {
+      const likeContainer = e.target.parentNode.parentNode.querySelector('.show');
+      await addLikes(e.target.id);
+      const newVal = +likeContainer.innerHTML + 1;
+      likeContainer.innerHTML = newVal;
+    });
+  });
+};
