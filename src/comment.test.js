6d45613379a9884c5480ae709eb37a8e9a8e2deb
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-11-04 15:23:49 +0430

    Add some changes to test

--- src/comment.test.js
+++ src/comment.test.js
@@ -1,25 +1,25 @@
-import commentsCounters from './modules/countComment.js';
+import counter from './modules/counter.js';
 import mockComment from './modules/__MockTest__/mockComment.js';
 
 describe('Test For the count function', () => {
   test('if array is empty', () => {
     const array = [];
-    expect(commentsCounters(array)).toBe(0);
+    expect(counter(array)).toBe(0);
   });
-  test('6 items', () => {
+  test('2 items', () => {
     const array = [{ idMeal: '52791' }, { idMeal: '52791' }];
-    expect(commentsCounters(array)).toBe(2);
+    expect(counter(array)).toBe(2);
   });
 });
 
 describe('Test For Commnet count', () => {
   test('Test For item_id 1', () => {
-    expect(commentsCounters(mockComment[0])).toBe(2);
+    expect(counter(mockComment[0].comment)).toBe(2);
   });
   test('Test For item_id 2', () => {
-    expect(commentsCounters(mockComment[1])).toBe(3);
+    expect(counter(mockComment[1].comment)).toBe(3);
   });
   test('Test For item_id 3', () => {
-    expect(commentsCounters(mockComment[2])).toBe(4);
+    expect(counter(mockComment[2].comment)).toBe(4);
   });
 });
